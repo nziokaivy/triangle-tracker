7efbd1f217eb35db97516df02d43021e93e7a0c7
nziokaivy <nziokaivy@gmail.com>
2018-11-30 14:12:44 +0300

    add control flows and logical operators

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function asignTriangleType () {
-  var sides = document.getElementsByClassName("inputs") [0].id;
+function asignTriangleType (side) {
+var sides = document.getElementsByClassName("inputs") [0].id;
   document.getElementById("output").innerHTML = X;
 
-var one = sides[0]
-var two = sides[1]
-var three = sides[2]
+var one = parseInt(document.getElementById("A"))
+var two = parseInt(document.getElementById("B"))
+var three = parseInt(document.getElementById("C"))
+
+if (one >= two += three || two >= one += three || three >= two += one ) {
+  output.innerHTML = ("This is NOT a triangle.")
+}
+else if (one !== two && one !== three && two !== three) {
+  output.innerHTML = ("This is a scalene triangle.")
+}
+else if (one === two && one !== three || one !== two && one === three || two === three && two !== one) {
+  output.innerHTML = ("This is an isosceles triangle.")
+}
+else (one === two && two === three) {
+  output.innerHTML = ("This is an equilateral triangle.")
+}
+}
+
+
+
+
 
 }
