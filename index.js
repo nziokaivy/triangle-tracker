e55f607ac46417dfdf7dd73831d030eb6790994e
nziokaivy <nziokaivy@gmail.com>
2018-11-30 11:57:29 +0300

    add variable names of the different input values in javascript file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function asignTriangleType () {
   var sides = document.getElementsByClassName("inputs") [0].id;
   document.getElementById("output").innerHTML = X;
+
+var one = sides[0]
+var two = sides[1]
+var three = sides[2]
+
 }
