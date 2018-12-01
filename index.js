13f4400930c676c04ee867a8dffd8cd74d3ab052
nziokaivy <nziokaivy@gmail.com>
2018-12-02 00:40:06 +0300

    add style to the background of the page

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function asignTriangleType (side) {
+function triangleType() {
 var sides = []
-sides.push(document.getElementsById("A").value);
-sides.push(document.getElementsById("B").value);
-sides.push(document.getElementsById("C").value);
+sides.push(document.getElementById("A").value);
+sides.push(document.getElementById("B").value);
+sides.push(document.getElementById("C").value);
 
-var one = parseInt(document.getElementById("A"))sides[0]
-var two = parseInt(document.getElementById("B"))sides[1]
-var three = parseInt(document.getElementById("C"))sides[2]
+var one = sides[0]
+var two = sides[1]
+var three = sides[2]
 
-if (one !== two && one !== three && two !== three) {
-  output.innerHTML = ("This is a scalene triangle.");
+if (one >= two+three || two >= one + three || three >= two + one) {
+  output.innerHTML = ("This is NOT a triangle.");
 }
 else if (one === two && one !== three || one !== two && one === three || two === three && two !== one) {
   output.innerHTML = ("This is an isosceles triangle.");
@@ -17,12 +17,7 @@ else if (one === two && one !== three || one !== two && one === three || two ===
 else if (one === two && two === three) {
  output.innerHTML = ("This is an equilateral triangle.");
 }
-else (one >= two+three && two >= one + three && three >= two + one) {
-  output.innerHTML = ("This is NOT a triangle.");
+else if (one !== two && one !== three && two !== three) {
+  output.innerHTML = ("This is a scalene triangle.");
 }
-}
-
-
-
-
 }
